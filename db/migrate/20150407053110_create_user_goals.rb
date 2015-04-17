34c5b3f5a7fd6eebfc1767733b7222127cf2de3f
luiz <luizchagasjr@gmail.com>
2015-04-17 16:31:12 -0500

    Fixing db:migrate

--- db/migrate/20150407053110_create_user_goals.rb
+++ db/migrate/20150407053110_create_user_goals.rb
@@ -1,8 +1,8 @@
 class CreateUserGoals < ActiveRecord::Migration
   def change
     create_table :user_goals do |t|
-      t.user :user
-      t.goal :goal
+      t.references :user
+      t.references :goal
 
       t.timestamps null: false
     end
