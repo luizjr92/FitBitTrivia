dc5dfb1c8076fcc84f4b7c2b5e2c109060b3a72c
luiz <luizchagasjr@gmail.com>
2015-05-06 01:57:20 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408231752) do
+ActiveRecord::Schema.define(version: 20150506060228) do
 
   create_table "authorizations", force: :cascade do |t|
     t.string   "uid"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150408231752) do
     t.string   "fullName"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "avatar"
   end
 
 end
